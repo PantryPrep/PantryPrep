3d7d90eb23cbaa3d7cb1300053f4b8242d5c0c1f
s-tc-chou <steve.tc.chou@gmail.com>
2016-08-18 12:24:21 -0500

    Added recipe list, retrofit  (SC 8/16/2016)

--- app/src/main/java/com/sonnytron/sortatech/pantryprep/Activity/HomeActivity.java
+++ app/src/main/java/com/sonnytron/sortatech/pantryprep/Activity/HomeActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -47,6 +48,13 @@ public class HomeActivity extends AppCompatActivity {
 
     private ActionBarDrawerToggle setupDrawerToggle() {
         return new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.drawer_open, R.string.drawer_close);
+    public void recipeActivityHandler(View view)
+    {
+//        Intent i = new Intent(this, RecipeLookupActivity.class);
+//        startActivity(i);
+        FragmentManager fm = getSupportFragmentManager();
+
+
     public void recipeActivityHandler(View view)
     {
 //        Intent i = new Intent(this, RecipeLookupActivity.class);
