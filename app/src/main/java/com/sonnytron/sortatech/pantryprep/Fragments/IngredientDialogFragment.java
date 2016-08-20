eeb5f37d80e8d4e232344f9936d4190b79e3d782
Sonny Rodriguez <sonny.rodriguez@manifest.com>
2016-08-20 10:12:11 -0500

    Added some more requirements to make sure the build is stable before we branch again for features - Added floating action button - Added dialog fragment for ingredient - Added icon drawable for adding ingredient - Added public interface callback for ingredient fragment

--- app/src/main/java/com/sonnytron/sortatech/pantryprep/Fragments/IngredientDialogFragment.java
+++ app/src/main/java/com/sonnytron/sortatech/pantryprep/Fragments/IngredientDialogFragment.java
@@ -1,6 +1,8 @@
 package com.sonnytron.sortatech.pantryprep.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.sonnytron.sortatech.pantryprep.Models.Ingredient;
 import com.sonnytron.sortatech.pantryprep.R;
@@ -24,9 +27,14 @@ public class IngredientDialogFragment extends DialogFragment {
     private Date expDate;
     private Button btSave;
     private Ingredient mIngredient;
+    private IngredientCallback mCallback;
 
     public IngredientDialogFragment() {
 
+    }
+
+    public interface IngredientCallback {
+        void saveIngredient(Ingredient ingredient);
     }
 
     public static IngredientDialogFragment newInstance(String title) {
@@ -53,6 +61,13 @@ public class IngredientDialogFragment extends DialogFragment {
 
         btSave = (Button) view.findViewById(R.id.btSaveIngredient);
 
+        btSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveIngredient();
+            }
+        });
+
         String title = getArguments().getString("title", "New Ingredient");
         getDialog().setTitle(title);
 
@@ -61,6 +76,11 @@ public class IngredientDialogFragment extends DialogFragment {
         getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mCallback = (IngredientCallback) context;
+    }
 
     public void onTypeRadioClicked(View view) {
         boolean checked = ((RadioButton) view).isChecked();
@@ -68,31 +88,50 @@ public class IngredientDialogFragment extends DialogFragment {
         switch (view.getId()) {
             case R.id.radioProtein:
                 if (checked) {
-
+                    mIngredient.setType("protein");
                 }
                 break;
             case R.id.radioVeggies:
                 if (checked) {
-
+                    mIngredient.setType("veggies");
                 }
                 break;
             case R.id.radioFruit:
                 if (checked) {
-
+                    mIngredient.setType("fruit");
                 }
                 break;
             case R.id.radioSpices:
                 if (checked) {
-
+                    mIngredient.setType("spices");
                 }
                 break;
             case R.id.radioDairy:
                 if (checked) {
-
+                    mIngredient.setType("dairy");
                 }
                 break;
             default:
 
         }
+    }
+
+    private void saveIngredient() {
+        Date mDate = new Date();
+        mDate.setTime(SystemClock.currentThreadTimeMillis());
+        mIngredient.setExpDate(mDate);
+        if (ingredientValidated()) {
+            mCallback.saveIngredient(mIngredient);
+            getDialog().dismiss();
+        } else {
+            Toast.makeText(getActivity(), "Please fill all required fields!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean ingredientValidated() {
+        return mIngredient.getTitle() != null &&
+                mIngredient.getTitle().length() > 0 &&
+                mIngredient.getType() != null &&
+                mIngredient.getType().length() > 0;
     }
 }
--- app/src/main/java/com/sonnytron/sortatech/pantryprep/Fragments/IngredientsListFragment.java
+++ app/src/main/java/com/sonnytron/sortatech/pantryprep/Fragments/IngredientsListFragment.java
@@ -1,6 +1,7 @@
 package com.sonnytron.sortatech.pantryprep.Fragments;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,12 +17,16 @@ import com.sonnytron.sortatech.pantryprep.R;
 public class IngredientsListFragment extends Fragment {
     private RecyclerView rvIngredients;
 
+    private FloatingActionButton btAddIngredient;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.ingredients_fragment, container, false);
 
         rvIngredients = (RecyclerView) view.findViewById(R.id.rvIngredients);
         rvIngredients.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        btAddIngredient = (FloatingActionButton) view.findViewById(R.id.float_add_ing);
 
         return view;
     }
